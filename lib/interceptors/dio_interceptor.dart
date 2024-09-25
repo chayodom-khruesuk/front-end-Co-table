4d624f46f98eb6a3bc75cb156c2b21096e69b292
krittamet-rrt <krittamet.rrt@gmail.com>
2024-09-25 09:29:49 +0700

    add: services, interceptor and create repositories file

---
+++ lib/interceptors/dio_interceptor.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:co_table/services/services.dart';
+
+class DioInterceptor extends Interceptor {
+  @override
+  void onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    final token = await Token.getToken();
+    if (token != null && token.isNotEmpty) {
+      options.headers['Authorization'] = 'Bearer $token';
+    }
+
+    options.headers['Content-Type'] = 'application/json';
+    options.baseUrl = 'http://localhost:8080';
+
+    super.onRequest(options, handler);
+  }
+}
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,1 +1,84 @@
+import 'package:co_table/interceptors/dio_interceptor.dart';
+import 'package:co_table/services/token.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
 
+class ApiService {
+  late final Dio _dio;
+
+  ApiService() {
+    _dio = Dio();
+    _dio.interceptors.add(DioInterceptor());
+  }
+
+  Future<Response> get(String path,
+      {Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response = await _dio.get(path, queryParameters: queryParameters);
+      return response;
+    } catch (e) {
+      debugPrint(e.toString());
+      rethrow;
+    }
+  }
+
+  Future<Response> post(String path, {Object? data}) async {
+    try {
+      final response = await _dio.post(path, data: data);
+      return response;
+    } catch (e) {
+      debugPrint(e.toString());
+      rethrow;
+    }
+  }
+
+  Future<Response> put(String path,
+      {Object? data, Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response =
+          await _dio.put(path, data: data, queryParameters: queryParameters);
+      return response;
+    } catch (e) {
+      debugPrint(e.toString());
+      rethrow;
+    }
+  }
+
+  Future<Response> delete(String path,
+      {Map<String, dynamic>? queryParameters}) async {
+    try {
+      final response =
+          await _dio.delete(path, queryParameters: queryParameters);
+      return response;
+    } catch (e) {
+      debugPrint(e.toString());
+      rethrow;
+    }
+  }
+
+  Future<String> login(
+      {required String username, required String password}) async {
+    try {
+      final response = await post('/token',
+          data: {'username': username, 'password': password});
+
+      if (response.statusCode == 200) {
+        final token = response.data['access_token'];
+        await Token.setToken(token);
+        return "Logged in successfully";
+      } else if (response.statusCode == 401) {
+        // detail: Incorrect username or password.
+        return response.data['detail'];
+      } else {
+        return "Something went wrong";
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+      return e.toString();
+    }
+  }
+
+  Future<void> logout() async {
+    await Token.clearToken();
+  }
+}
