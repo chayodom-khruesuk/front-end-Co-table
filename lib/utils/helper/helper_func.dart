c7f4533f973c7bf7a44678237381d2193bacd798
Chayodom-Khruesuk <chayodom.kx@gmail.com>
2024-10-02 01:42:25 +0700

    fix: Problem with deactive widget

--- lib/utils/helper/helper_func.dart
+++ lib/utils/helper/helper_func.dart
@@ -6,40 +6,48 @@ class SnackBarHelper {
   static void showSuccessSnackBar(BuildContext context,
       {required String title,
       required String message,
-      required Duration duration}) {
+      Duration duration = const Duration(seconds: 3)}) {
     _showSnackBar(context,
         title: title,
         message: message,
         backgroundColor: Colors.green,
-        icon: LineAwesomeIcons.check_circle);
+        icon: LineAwesomeIcons.check_circle,
+        duration: duration);
   }
 
   static void showWarningSnackBar(BuildContext context,
       {required String title,
       required String message,
-      required Duration duration}) {
+      Duration duration = const Duration(seconds: 3)}) {
     _showSnackBar(context,
         title: title,
         message: message,
         backgroundColor: Colors.orange,
-        icon: LineAwesomeIcons.exclamation_circle_solid);
+        icon: LineAwesomeIcons.exclamation_circle_solid,
+        duration: duration);
   }
 
   static void showErrorSnackBar(BuildContext context,
       {required String title,
       required String message,
-      required Duration duration}) {
+      Duration duration = const Duration(seconds: 3)}) {
     _showSnackBar(context,
         title: title,
         message: message,
         backgroundColor: Colors.red.shade600,
-        icon: LineAwesomeIcons.times_circle);
+        icon: LineAwesomeIcons.times_circle,
+        duration: duration);
   }
 
   static void showModernSnackBar(BuildContext context,
-      {required String title, required String message}) {
+      {required String title,
+      required String message,
+      Duration duration = const Duration(seconds: 3)}) {
     _showSnackBar(context,
-        title: title, message: message, backgroundColor: Colors.blueGrey);
+        title: title,
+        message: message,
+        backgroundColor: Colors.blueGrey,
+        duration: duration);
   }
 
   static void _showSnackBar(
@@ -48,39 +56,46 @@ class SnackBarHelper {
     required String message,
     required Color backgroundColor,
     IconData? icon,
+    required Duration duration,
   }) {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Row(
-          children: [
-            if (icon != null) Icon(icon, color: Colors.white),
-            const SizedBox(width: 10),
-            Expanded(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(title,
-                      style: const TextStyle(fontWeight: FontWeight.bold)),
-                  Text(message),
-                ],
-              ),
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (context.mounted) {
+        final scaffoldMessenger = ScaffoldMessenger.of(context);
+        scaffoldMessenger.showSnackBar(
+          SnackBar(
+            content: Row(
+              children: [
+                if (icon != null) Icon(icon, color: Colors.white),
+                const SizedBox(width: 10),
+                Expanded(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(title,
+                          style: const TextStyle(fontWeight: FontWeight.bold)),
+                      Text(message),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
-        backgroundColor: backgroundColor,
-        duration: const Duration(seconds: 3),
-        behavior: SnackBarBehavior.floating,
-        margin: const EdgeInsets.all(SizeConstant.defaultPadding - 10),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        action: SnackBarAction(
-          label: 'Dismiss',
-          textColor: Colors.white,
-          onPressed: () {
-            ScaffoldMessenger.of(context).hideCurrentSnackBar();
-          },
-        ),
-      ),
-    );
+            backgroundColor: backgroundColor,
+            duration: duration,
+            behavior: SnackBarBehavior.floating,
+            margin: const EdgeInsets.all(SizeConstant.defaultPadding - 10),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            action: SnackBarAction(
+              label: 'Dismiss',
+              textColor: Colors.white,
+              onPressed: () {
+                scaffoldMessenger.hideCurrentSnackBar();
+              },
+            ),
+          ),
+        );
+      }
+    });
   }
 }
